0f8776c168605728542b9d47547f0e6c5013ea17
Paul McCaughtry <paul.mccaughtry@gazzang.com>
2013-09-28 22:24:27 -0500

    first commit

---
+++ app/js/directives/charts.js
@@ -0,0 +1,25 @@
+;(function () {
+	'use strict';
+	/* 
+	 * Use of functional strict mode causes scoping issues when declaring the function or creating as an expression.
+	 * The use of angular.module (below) is needed to remedy.
+	*/
+	
+	var chart = angular.module('Charts', []);
+
+	chart.directive('pmChart', function () {
+		return {
+			restrict: 'EA',
+			controller: ['$scope', '$attrs', function ($scope, $attrs) {
+				var plotid = '#' + $attrs.id,
+					model = $scope[$attrs.ngModel];
+
+				// add attribute to check type, populate default options but also pull in custom options from user?
+
+				$scope.$watch('model', function (x) {
+					$.plot(plotid, x.data, x.options);
+				});
+			}]
+		};
+	});
+}());
